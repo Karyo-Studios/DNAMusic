08f4e595230f5f670b6c355aaa8183550a8ac8be
Dan Gorelick <dqgorelick@gmail.com>
2023-08-09 13:02:00 -0700

    fixes build

--- src/graphics.js
+++ src/graphics.js
@@ -1,35 +1,5 @@
 import React, { useCallback } from "react";
 
-export const Tile = (props) => {
-  const draw = useCallback(
-    (g) => {
-      g.clear();
-      //  g.beginFill(`hsl(192,0%,${props.noteColor}%)`);
-      g.lineStyle(props.height / 15, props.color, 1);
-      g.drawRoundedRect(2, 0, props.width, props.height, 3);
-      g.endFill();
-    },
-    [props]
-  );
-  return <Graphics draw={draw} />;
-};
-
-export const PlayheadTile = (props) => {
-  const draw = useCallback(
-    (g) => {
-      g.clear();
-      g.beginFill(props.color);
-      g.lineStyle(props.height / 15, 0xffffff, 1);
-      g.drawRoundedRect(2, 0, props.width, props.height, 3);
-      g.endFill();
-    },
-    [props]
-  );
-  return <Graphics draw={draw} />;
-};
-
-let version = 0;
-
 export const useAnimationFrame = (callback) => {
   // Use useRef for mutable variables that we want to persist
   // without triggering a re-render on their change
@@ -39,7 +9,7 @@ export const useAnimationFrame = (callback) => {
   const animate = (time) => {
     if (previousTimeRef.current != undefined) {
       const deltaTime = time - previousTimeRef.current;
-      callback.current(deltaTime, version);
+      callback.current(deltaTime);
     }
     previousTimeRef.current = time;
     requestRef.current = requestAnimationFrame(animate);
